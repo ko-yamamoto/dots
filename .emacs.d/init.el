b6d9ab0e7b8bf6bcecb8ec64bd2618cb4f63b005
nishikawasasaki <kawaisosu.sosu@gmail.com>
2011-09-15 01:01:42 +0900

    change emacs setting   add clojure-mode.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -995,6 +995,13 @@ interpreter-mode-alist))
 
 
 
+;;====================
+;; clojure-mode
+;;====================
+(require 'clojure-mode)
+(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+(autoload 'clojure-mode "clojure-mode" "A major mode for Clojure" t)
+
 
 ;; ---------------------------------------------------------------------------------
 ;; elisp Settings
