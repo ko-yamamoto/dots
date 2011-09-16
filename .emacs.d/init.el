734e06b675cb55d43f7d4d14a347ff9950cc0238
nishikawasasaki <kawaisosu.sosu@gmail.com>
2011-09-16 09:53:41 +0900

    change emacs setting   add rst.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1700,6 +1700,21 @@ interpreter-mode-alist))
 (cygwin-mount-activate)
 
 
+
+;;====================
+;; cyg-mount
+;;====================
+(require 'rst)
+;; 拡張子の*.rst, *.restのファイルをrst-modeで開く
+(setq auto-mode-alist
+      (append '(("\\.rst$" . rst-mode)
+		("\\.rest$" . rst-mode)) auto-mode-alist))
+;; 背景が黒い場合はこうしないと見出しが見づらい
+;; (setq frame-background-mode 'dark)
+;; 全部スペースでインデントしましょう
+(add-hook 'rst-mode-hook '(lambda() (setq indent-tabs-mode nil)))
+
+
 ;; ---------------------------------------------------------------------------------
 ;; Visual Settings 
 ;; ---------------------------------------------------------------------------------
