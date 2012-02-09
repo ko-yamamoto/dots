d576765e452b9e30aa32c2f626f6da7349f3e44a
nishikawasasaki <kawaisosu.sosu@gmail.com>
2012-02-10 07:58:10 +0900

    change emacs setting        commented malabar dont use        use cygwin-mount

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1766,8 +1766,9 @@
 ;; cyg-mount
 ;;====================
 ;; Cygwin のドライブ・プレフィックスを有効に
-;; (require 'cygwin-mount)
-;; (cygwin-mount-activate)
+(setq cygwin-mount-cygwin-bin-directory "c:/cygwin/bin")
+(require 'cygwin-mount)
+(cygwin-mount-activate)
 
 
 
@@ -1890,24 +1891,24 @@
 ;;====================
 ;; malabar-mode
 ;;====================
-(require 'malabar-mode nil t)
-(setq malabar-groovy-lib-dir (expand-file-name "~/.emacs.d/elisp/malabar-1.5/malabar-lib")) ; お好みで
-(add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
+;; (require 'malabar-mode nil t)
+;; (setq malabar-groovy-lib-dir (expand-file-name "~/.emacs.d/elisp/malabar-1.5/malabar-lib")) ; お好みで
+;; (add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
 
-;; 普段使わないパッケージを import 候補から除外
-(add-to-list 'malabar-import-excluded-classes-regexp-list
-             "^java\\.awt\\..*$")
-(add-to-list 'malabar-import-excluded-classes-regexp-list
-             "^com\\.sun\\..*$")
-(add-to-list 'malabar-import-excluded-classes-regexp-list
-             "^org\\.omg\\..*$")
+;; ;; 普段使わないパッケージを import 候補から除外
+;; (add-to-list 'malabar-import-excluded-classes-regexp-list
+;;              "^java\\.awt\\..*$")
+;; (add-to-list 'malabar-import-excluded-classes-regexp-list
+;;              "^com\\.sun\\..*$")
+;; (add-to-list 'malabar-import-excluded-classes-regexp-list
+;;              "^org\\.omg\\..*$")
 
-;; コンパイル前に保存する
-(add-hook 'malabar-mode-hook
-          (lambda ()
-            (add-hook 'after-save-hook 'malabar-compile-file-silently nil t)))
-; 日本語だとコンパイルエラーメッセージが化けるので
-(setq malabar-groovy-java-options '("-Duser.language=en")) 
+;; ;; コンパイル前に保存する
+;; (add-hook 'malabar-mode-hook
+;;           (lambda ()
+;;             (add-hook 'after-save-hook 'malabar-compile-file-silently nil t)))
+;; ; 日本語だとコンパイルエラーメッセージが化けるので
+;; (setq malabar-groovy-java-options '("-Duser.language=en")) 
 
 
 ;;====================
