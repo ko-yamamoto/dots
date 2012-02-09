945e19e9d7a080a8fbfbfaa62ea84b9c4ddefe11
nishikawasasaki <kawaisosu.sosu@gmail.com>
2012-02-10 07:38:01 +0900

    change emacs setting        key map

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1835,7 +1835,7 @@
 (global-set-key "\M-r" 'gtags-find-rtag)    ;関数の参照先へ
 (global-set-key "\M-s" 'gtags-find-symbol)  ;変数の定義元/参照先へ
 (global-set-key "\M-p" 'gtags-find-pattern)
-(global-set-key "\M-f" 'gtags-find-file)    ;ファイルにジャンプ
+;;(global-set-key "\M-f" 'gtags-find-file)    ;ファイルにジャンプ
 (global-set-key [?\C-,] 'gtags-pop-stack)   ;前のバッファに戻る
 
 (setq gtags-mode-hook
