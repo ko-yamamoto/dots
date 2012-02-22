ec5e28a25b58d1582cabc8532510ef2b78eb59c6
nishikawasasaki <kawaisosu.sosu@gmail.com>
2012-02-23 03:32:49 +0900

    change emacs setting        add function for dired            openfile or dir in accordance with the situation

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -305,17 +305,26 @@
       (kill-buffer my-dired-before-buffer)))
 
 
+
+;; ファイルなら別バッファで、ディレクトリなら同じバッファで開く
+(defun dired-open-in-accordance-with-situation ()
+  (interactive)
+  (let ((file (dired-get-filename)))
+    (if (file-directory-p file)
+        (dired-find-alternate-file)
+      (dired-find-file))))
+
 ;; dired-find-alternate-file の有効化
 (put 'dired-find-alternate-file 'disabled nil)
 ;; RET 標準の dired-find-file では dired バッファが複数作られるので
 ;; dired-find-alternate-file を代わりに使う
-(define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
+(define-key dired-mode-map (kbd "RET") 'dired-open-in-accordance-with-situation)
 (define-key dired-mode-map (kbd "a") 'dired-find-file)
 
 
 ;; ディレクトリの移動キーを追加(wdired 中は無効)
 (define-key dired-mode-map (kbd "<left>") 'dired-up-directory)
-(define-key dired-mode-map (kbd "<right>") 'dired-find-alternate-file)
+(define-key dired-mode-map (kbd "<right>") 'dired-open-in-accordance-with-situation)
 
 
 ;; フルパスファイル名コピー(ファイル名だけは"w")
