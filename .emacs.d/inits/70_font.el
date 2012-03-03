15956a85879d7213b799bb6b6190e6ae8328bdc7
nishikawasasaki <kawaisosu.sosu@gmail.com>
2012-03-03 16:07:06 +0900

    change emacs setting     font size for Win

--- .emacs.d/inits/70_font.el
+++ .emacs.d/inits/70_font.el
@@ -21,7 +21,7 @@
                     '("MeiryoKe_Console" . "unicode-bmp"))
 )
 (when is_win
-  (set-default-font "ricty-10")
+  (set-default-font "ricty-10.5")
   (set-fontset-font (frame-parameter nil 'font)
                     'japanese-jisx0208
                     '("ricty" . "unicode-bmp"))
