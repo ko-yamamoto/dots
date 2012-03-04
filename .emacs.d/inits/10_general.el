b48e1c8290d0dce149a0d11c2216c4b6fb622c45
nishikawasasaki <kawaisosu.sosu@gmail.com>
2012-03-05 02:20:39 +0900

    change emacs setting     change wrap setting

--- .emacs.d/inits/10_general.el
+++ .emacs.d/inits/10_general.el
@@ -73,30 +73,7 @@
 (setq recentf-max-menu-items 30)
 
 
-;; 行のどこにカーソルがあっても行全体削除
-(defun kill-whole-line (&optional numlines)
-  (interactive "p")
-  (setq pos (current-column))
-  (beginning-of-line)
-  (kill-line numlines)
-  (move-to-column pos))
-(global-set-key (kbd "M-k") 'kill-whole-line)
-
-
-;; "フォーマット"
-(defun format-line-indent ()
-  "バッファ全体のインデントを整える"
-  (interactive)
-  (indent-region (point-min) (point-max)))
-(global-set-key (kbd "C-S-f") 'kill-whole-line)
-
-
-
-
-
-
-
-                                        ; ごみ箱を有効
+;; ごみ箱を有効
 (setq delete-by-moving-to-trash t)
 
 ;; バックアップファイルを作らない
@@ -232,131 +209,12 @@
 (global-set-key (kbd "M-Y") 'copy-line)
 
 
-;; M-↑などで今の行をコピー
-(defun duplicate-line-backward ()
-  "Duplicate the current line backward."
-  (interactive "*")
-  (save-excursion
-    (let ((contents
-           (buffer-substring
-            (line-beginning-position)
-            (line-end-position))))
-      (beginning-of-line)
-      (insert contents ?\n)))
-  (previous-line 1))
-
-(defun duplicate-region-backward ()
-  "If mark is active duplicates the region backward."
-  (interactive "*")
-  (if mark-active
-      (let* (
-             (deactivate-mark nil)
-             (start (region-beginning))
-             (end (region-end))
-             (contents (buffer-substring
-                        start
-                        end)))
-        (save-excursion
-          (goto-char start)
-          (insert contents))
-        (goto-char end)
-        (push-mark (+ end (- end start))))
-    (error
-     "Mark is not active. Region not duplicated.")))
-
-(defun duplicate-line-forward ()
-  "Duplicate the current line forward."
-  (interactive "*")
-  (save-excursion
-    (let ((contents (buffer-substring
-                     (line-beginning-position)
-                     (line-end-position))))
-      (end-of-line)
-      (insert ?\n contents)))
-  (next-line 1))
-
-(defun duplicate-region-forward ()
-  "If mark is active duplicates the region forward."
-  (interactive "*")
-  (if mark-active
-      (let* (
-             (deactivate-mark nil)
-             (start (region-beginning))
-             (end (region-end))
-             (contents (buffer-substring
-                        start
-                        end)))
-        (save-excursion
-          (goto-char end)
-          (insert contents))
-        (goto-char start)
-        (push-mark end)
-        (exchange-point-and-mark))
-    (error "Mark is not active. Region not duplicated.")))
-
-(global-set-key [M-up]    'duplicate-line-backward)
-(global-set-key [M-down]  'duplicate-line-forward)
-(global-set-key [M-right] 'duplicate-region-forward)
-(global-set-key [M-left]  'duplicate-region-backward)
-
-
-;; 折り返し表示をトグル
-(defun toggle-truncate-lines ()
-  "折り返し表示をトグル動作します."
-  (interactive)
-  (if truncate-lines
-      (setq truncate-lines nil)
-    (setq truncate-lines t))
-  (recenter))
-(global-set-key (kbd "C-c C-l") 'toggle-truncate-lines) ; 折り返し表示ON/OFF
-
-
-;; my window resize
-(defun my-window-resizer ()
-  "Control window size and position."
-  (interactive)
-  (let ((window-obj (selected-window))
-        (current-width (window-width))
-        (current-height (window-height))
-        (dx (if (= (nth 0 (window-edges)) 0) 1
-              -1))
-        (dy (if (= (nth 1 (window-edges)) 0) 1
-              -1))
-        action c)
-    (catch 'end-flag
-      (while t
-        (setq action
-              (read-key-sequence-vector (format "size[%dx%d]"
-                                                (window-width)
-                                                (window-height))))
-        (setq c (aref action 0))
-        (cond ((= c ?l)
-               (enlarge-window-horizontally dx))
-              ((= c ?h)
-               (shrink-window-horizontally dx))
-              ((= c ?j)
-               (enlarge-window dy))
-              ((= c ?k)
-               (shrink-window dy))
-              ;; otherwise
-              (t
-               ;;               (let ((last-command-char (aref action 0))
-               (let ((last-command-event (aref action 0))
-                     (command (key-binding action)))
-                 (when command
-                   (call-interactively command)))
-               (message "Quit")
-               (throw 'end-flag t)))))))
-
-
 ;; C-q -> pre-fix key
 (define-key global-map "\C-q" (make-sparse-keymap))
 
 ;; quoted-insert -> C-q C-q
 (global-set-key "\C-q\C-q" 'quoted-insert)
 
-;; window-resizer C-q C-r (resize)
-(global-set-key "\C-q\C-r" 'my-window-resizer)
 
 ;; window move
 (global-set-key "\C-ql" 'windmove-right)
@@ -368,27 +226,6 @@
 (global-set-key "\C-q1" 'delete-other-windows)
 (global-set-key "\C-q2" 'split-window-vertically)
 (global-set-key "\C-q3" 'split-window-horizontally)
-(defun split-for-twmode ()
-
-  "現在のウィンドウを3等分する関数"
-  (interactive)
-  (progn
-    (split-window-horizontally)
-    (other-window 1)
-    (split-window-vertically)
-    (enlarge-window 7)
-    (windmove-left)
-    (twit)
-    (windmove-right)
-    (twit)
-    (windmove-down)
-    (twittering-replies-timeline)
-    (windmove-up)
-))
-(global-set-key "\C-q4" 'split-for-twmode)
-
-(global-set-key "\C-qtt" 'twit)
-
 
 
 ;; 自動でchmod+x
@@ -405,4 +242,7 @@
 (add-hook 'after-save-hook 'make-file-executable)
 
 
-
+;; 折り返しあり
+(setq truncate-lines nil)
+;; 画面分割してもデフォルトで折り返す
+(setq truncate-partial-width-windows nil)
---
+++ .emacs.d/inits/80_my-func.el
@@ -0,0 +1,157 @@
+;; 行のどこにカーソルがあっても行全体削除
+(defun kill-whole-line (&optional numlines)
+  (interactive "p")
+  (setq pos (current-column))
+  (beginning-of-line)
+  (kill-line numlines)
+  (move-to-column pos))
+(global-set-key (kbd "M-k") 'kill-whole-line)
+
+
+;; "フォーマット"
+(defun format-line-indent ()
+  "バッファ全体のインデントを整える"
+  (interactive)
+  (indent-region (point-min) (point-max)))
+(global-set-key (kbd "C-S-f") 'kill-whole-line)
+
+
+;; M-↑などで今の行をコピー
+(defun duplicate-line-backward ()
+  "Duplicate the current line backward."
+  (interactive "*")
+  (save-excursion
+    (let ((contents
+           (buffer-substring
+            (line-beginning-position)
+            (line-end-position))))
+      (beginning-of-line)
+      (insert contents ?\n)))
+  (previous-line 1))
+
+(defun duplicate-region-backward ()
+  "If mark is active duplicates the region backward."
+  (interactive "*")
+  (if mark-active
+      (let* (
+             (deactivate-mark nil)
+             (start (region-beginning))
+             (end (region-end))
+             (contents (buffer-substring
+                        start
+                        end)))
+        (save-excursion
+          (goto-char start)
+          (insert contents))
+        (goto-char end)
+        (push-mark (+ end (- end start))))
+    (error
+     "Mark is not active. Region not duplicated.")))
+
+(defun duplicate-line-forward ()
+  "Duplicate the current line forward."
+  (interactive "*")
+  (save-excursion
+    (let ((contents (buffer-substring
+                     (line-beginning-position)
+                     (line-end-position))))
+      (end-of-line)
+      (insert ?\n contents)))
+  (next-line 1))
+
+(defun duplicate-region-forward ()
+  "If mark is active duplicates the region forward."
+  (interactive "*")
+  (if mark-active
+      (let* (
+             (deactivate-mark nil)
+             (start (region-beginning))
+             (end (region-end))
+             (contents (buffer-substring
+                        start
+                        end)))
+        (save-excursion
+          (goto-char end)
+          (insert contents))
+        (goto-char start)
+        (push-mark end)
+        (exchange-point-and-mark))
+    (error "Mark is not active. Region not duplicated.")))
+
+(global-set-key [M-up]    'duplicate-line-backward)
+(global-set-key [M-down]  'duplicate-line-forward)
+(global-set-key [M-right] 'duplicate-region-forward)
+(global-set-key [M-left]  'duplicate-region-backward)
+
+
+;; 折り返し表示をトグル
+(defun toggle-truncate-lines ()
+  "折り返し表示をトグル動作します."
+  (interactive)
+  (if truncate-lines
+      (setq truncate-lines nil)
+    (setq truncate-lines t))
+  (recenter))
+(global-set-key (kbd "C-c C-l") 'toggle-truncate-lines) ; 折り返し表示ON/OFF
+(key-chord-define-global "cl" 'toggle-truncate-lines)
+
+
+;; my window resize
+(defun my-window-resizer ()
+  "Control window size and position."
+  (interactive)
+  (let ((window-obj (selected-window))
+        (current-width (window-width))
+        (current-height (window-height))
+        (dx (if (= (nth 0 (window-edges)) 0) 1
+              -1))
+        (dy (if (= (nth 1 (window-edges)) 0) 1
+              -1))
+        action c)
+    (catch 'end-flag
+      (while t
+        (setq action
+              (read-key-sequence-vector (format "size[%dx%d]"
+                                                (window-width)
+                                                (window-height))))
+        (setq c (aref action 0))
+        (cond ((= c ?l)
+               (enlarge-window-horizontally dx))
+              ((= c ?h)
+               (shrink-window-horizontally dx))
+              ((= c ?j)
+               (enlarge-window dy))
+              ((= c ?k)
+               (shrink-window dy))
+              ;; otherwise
+              (t
+               ;;               (let ((last-command-char (aref action 0))
+               (let ((last-command-event (aref action 0))
+                     (command (key-binding action)))
+                 (when command
+                   (call-interactively command)))
+               (message "Quit")
+               (throw 'end-flag t)))))))
+;; window-resizer C-q C-r (resize)
+(global-set-key "\C-q\C-r" 'my-window-resizer)
+
+
+
+(defun split-for-twmode ()
+  "現在のウィンドウを3等分する関数"
+  (interactive)
+  (progn
+    (split-window-horizontally)
+    (other-window 1)
+    (split-window-vertically)
+    (enlarge-window 7)
+    (windmove-left)
+    (twit)
+    (windmove-right)
+    (twit)
+    (windmove-down)
+    (twittering-replies-timeline)
+    (windmove-up)
+))
+(global-set-key "\C-q4" 'split-for-twmode)
+
