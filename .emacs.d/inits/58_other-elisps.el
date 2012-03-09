abc93ee4f89c6155e4aee1452a3c97eadde01315
nishikawasasaki <kawaisosu.sosu@gmail.com>
2012-03-09 14:16:52 +0900

    change emacs setting        change junk file dir

--- .emacs.d/inits/58_other-elisps.el
+++ .emacs.d/inits/58_other-elisps.el
@@ -129,14 +129,14 @@
 ;;====================
 ;; cl-indent-patches
 ;;====================
-(when (require 'cl-indent-patches nil t)
-  ;; emacs-lispのインデントと混同しないように
-  (setq lisp-indent-function
-        (lambda (&rest args)
-          (apply (if (memq major-mode '(emacs-lisp-mode lisp-interaction-mode))
-                     'lisp-indent-function
-                   'common-lisp-indent-function)
-                 args))))
+;; (when (require 'cl-indent-patches nil t)
+;;   ;; emacs-lispのインデントと混同しないように
+;;   (setq lisp-indent-function
+;;         (lambda (&rest args)
+;;           (apply (if (memq major-mode '(emacs-lisp-mode lisp-interaction-mode))
+;;                      'lisp-indent-function
+;;                    'common-lisp-indent-function)
+;;                  args))))
 
 
 ;;====================
