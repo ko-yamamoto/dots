62a1a2c783de505041f1b2fedf880353650ea9f7
nishikawasasaki <kawaisosu.sosu@gmail.com>
2012-03-26 02:33:58 +0900

    modify screen envoding setting

--- .emacs.d/inits/10_general.el
+++ .emacs.d/inits/10_general.el
@@ -34,7 +34,8 @@
 ;; language & code
 (set-language-environment 'Japanese)
 (prefer-coding-system 'utf-8)
-
+(set-keyboard-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
 
 ;; beepを消す
 (setq visible-bell t)
