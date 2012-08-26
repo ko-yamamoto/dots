caa0a50738234668db22a5b26023e4e384d7a6d8
nishikawasasaki <kawaisosu.sosu@gmail.com>
2012-08-26 22:54:41 +0900

    change emacs setting     update scala-mode

--- .emacs.d/inits/30_scala-mode.el
+++ .emacs.d/inits/30_scala-mode.el
@@ -1,10 +1,13 @@
 ;;====================
 ;; scala-mode
 ;;====================
-(add-to-list 'load-path "~/.emacs.d/snippets/scala-mode")
+(add-to-list 'load-path "~/.emacs.d/elisp/scala-mode")
 (require 'scala-mode-auto)
 (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
+
 (require 'scala-mode-feature-electric)
+
+
 (add-hook 'scala-mode-hook
           (lambda ()
             (scala-electric-mode)
@@ -37,17 +40,17 @@
 
 
 ;; ;; インデント修正
-;; (defadvice scala-block-indentation (around improve-indentation-after-brace activate)
-;;   (if (eq (char-before) ?\{)
-;;       (setq ad-return-value (+ (current-indentation) scala-mode-indent:step))
-;;     ad-do-it))
-;; (defun scala-newline-and-indent ()
-;;   (interactive)
-;;   (delete-horizontal-space)
-;;   (let ((last-command nil))
-;;     (newline-and-indent))
-;;   (when (scala-in-multi-line-comment-p)
-;;     (insert "* ")))
-;; (add-hook 'scala-mode-hook
-;;           (lambda ()
-;;             (define-key scala-mode-map (kbd "RET") 'scala-newline-and-indent)))
+(defadvice scala-block-indentation (around improve-indentation-after-brace activate)
+  (if (eq (char-before) ?\{)
+      (setq ad-return-value (+ (current-indentation) scala-mode-indent:step))
+    ad-do-it))
+(defun scala-newline-and-indent ()
+  (interactive)
+  (delete-horizontal-space)
+  (let ((last-command nil))
+    (newline-and-indent))
+  (when (scala-in-multi-line-comment-p)
+    (insert "* ")))
+(add-hook 'scala-mode-hook
+          (lambda ()
+            (define-key scala-mode-map (kbd "RET") 'scala-newline-and-indent)))
