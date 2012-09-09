e03103ec4333f60ded706c3b17d9c4479cf433ff
nishikawasasaki <kawaisosu.sosu@gmail.com>
2012-09-09 15:56:38 +0900

    change emacs setting * enable scratch-log

--- .emacs.d/inits/50_el-get.el
+++ .emacs.d/inits/50_el-get.el
@@ -392,7 +392,7 @@
                           (setq sl-prev-scratch-string-file "~/.emacs.d/.scratch-log-prev")
 
                           ;; nil なら emacs 起動時に，最後に終了したときの スクラッチバッファの内容を復元しない。初期値は t です。
-                          (setq sl-restore-scratch-p nil)
+                          (setq sl-restore-scratch-p t)
                           ;; nil なら スクラッチバッファを削除できるままにする。初期値は t です。
                           (setq sl-prohibit-kill-scratch-buffer-p t)
                           ))
