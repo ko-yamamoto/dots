7cd93bd07cb789b284007a7b65c38c5865e0d348
nishikawasasaki <kawaisosu.sosu@gmail.com>
2012-09-12 23:00:09 +0900

    change emacs settings

--- .emacs.d/inits/10_general.el
+++ .emacs.d/inits/10_general.el
@@ -234,6 +234,26 @@
 (global-set-key (kbd "C-q 0") 'window-toggle-division)
 
 
+
+(defun reopen-file ()
+  "undo 可能なようにファイルを開き直す"
+  (interactive)
+  (let ((file-name (buffer-file-name))
+        (old-supersession-threat
+         (symbol-function 'ask-user-about-supersession-threat))
+        (point (point)))
+    (when file-name
+      (fset 'ask-user-about-supersession-threat (lambda (fn)))
+      (unwind-protect
+          (progn
+            (erase-buffer)
+            (insert-file file-name)
+            (set-visited-file-modtime)
+            (goto-char point))
+        (fset 'ask-user-about-supersession-threat
+              old-supersession-threat)))))
+(global-set-key (kbd "C-x C-r") 'reopen-file)
+
 ;; ウィンドウ移動を楽に
 ;; (define-key global-map (kbd "C-t") 'other-window)
 (defun other-window-or-split ()
