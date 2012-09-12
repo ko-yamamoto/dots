c3f229c45ee37f7baebea96c684d17bcfd84c88a
nishikawasasaki <kawaisosu.sosu@gmail.com>
2012-09-12 23:00:09 +0900

    change emacs settings

--- .emacs.d/inits/70_font.el
+++ .emacs.d/inits/70_font.el
@@ -23,11 +23,14 @@
   ;; (set-fontset-font (frame-parameter nil 'font)
   ;;                   'japanese-jisx0208
   ;;                   '("VL ゴシック" . "unicode-bmp"))
-  (set-default-font "ricty-10.5")
-  (set-face-font 'variable-pitch "ricty-10.5")
+  ;; (set-default-font "ricty-9")
+  (set-default-font "VL ゴシック-9")
+  ;; (set-face-font 'variable-pitch "ricty-9")
+  (set-face-font 'variable-pitch "VL ゴシック-9")
   (set-fontset-font (frame-parameter nil 'font)
                     'japanese-jisx0208
-                    '("ricty-10.5" . "unicode-bmp"))
+                    ;; '("ricty-9" . "unicode-bmp"))
+                    '("VL ゴシック" . "unicode-bmp"))
 
 )
 (when is_win
