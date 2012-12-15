2a40553f82bdaebaf38c76a19dbc0e65140b3203
nishikawasasaki <kawaisosu.sosu@gmail.com>
2012-12-16 00:27:49 +0900

    delete setting

--- .emacs.d/inits/65_other-elisps.el
+++ .emacs.d/inits/65_other-elisps.el
@@ -296,41 +296,6 @@
 
 
 
-;;====================
-;; pomodoro.el
-;;====================
-(require 'pomodoro)
-;; 作業時間終了後に開くファイル。デフォルトでは "~/.emacs.d/pomodoro.org"
-(setq pomodoro:file "~/howm/2012/03/2012-03-06-110154.howm") ; howm の TODO リスト
-
-;; 作業時間関連
-(setq pomodoro:work-time 25
-      pomodoro:rest-time 5
-      pomodoro:long-rest-time 30)
-
-;; hook関数関連
-(require 'notifications) ;; Linuxで DBUSがある環境のみ
-(defun* my/pomodoro-notification (&key (title "Pomodoro")
-                                       body
-                                       (urgency 'critical))
-  (notifications-notify :title title :body body :urgency urgency))
-
-;; 作業終了後の hook
-(add-hook 'pomodoro:finish-work-hook
-          (lambda ()
-            (my/pomodoro-notification :body "Work is Finish")))
-
-;; 休憩終了後の hook
-(add-hook 'pomodoro:finish-rest-hook
-          (lambda ()
-            (my/pomodoro-notification :body "Break time is finished")))
-
-;; 長期休憩後の hook
-(add-hook 'pomodoro:long-rest-hook
-          (lambda ()
-            (my/pomodoro-notification :body "Long Break time now")))
-
-
 
 ;;====================
 ;; others
