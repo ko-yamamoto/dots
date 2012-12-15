ad942da104b92a129266136328f5b35b12cb615f
nishikawasasaki <kawaisosu.sosu@gmail.com>
2012-12-15 23:36:26 +0900

    change emacs settings

--- .emacs.d/inits/20_el-get.el
+++ .emacs.d/inits/20_el-get.el
@@ -32,9 +32,9 @@
 
                           ;; configuration helm variable
                           (setq helm-idle-delay 0.2)
-                          (setq helm-input-idle-delay 0)
-                          (setq helm-candidate-number-limit 300)
-                          (setq helm-buffer-max-length 100)
+                          (setq helm-input-idle-delay 0.1) ; 入力後に候補を更新するまでの時間
+                          (setq helm-candidate-number-limit 300) ; 候補数
+                          (setq helm-buffer-max-length 100) ; バッファ名の最大長
 
                           (require 'helm-files)
 
@@ -58,6 +58,7 @@
                               (persistent-help
                                . "Kill this buffer / C-u \\[helm-execute-persistent-action]: Show this buffer")))
 
+
                           (defun helm-my ()
                             (interactive)
                             (helm-other-buffer '(helm-c-source-buffers-list-R
@@ -71,6 +72,40 @@
                           (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 
                           (global-set-key (kbd "C-q i") 'helm-imenu)
+
+
+                          ;; git 管理ファイルを状態に応じて表示
+                          (defun helm-c-sources-git-project-for (pwd)
+                            (loop for elt in
+                                  '(("Modified files" . "--modified")
+                                    ("Untracked files" . "--others --exclude-standard")
+                                    ("All controlled files in this project" . nil))
+                                  for title  = (format "%s (%s)" (car elt) pwd)
+                                  for option = (cdr elt)
+                                  for cmd    = (format "git ls-files %s" (or option ""))
+                                  collect
+                                  `((name . ,title)
+                                    (init . (lambda ()
+                                              (unless (and (not ,option) (helm-candidate-buffer))
+                                                (with-current-buffer (helm-candidate-buffer 'global)
+                                                  (call-process-shell-command ,cmd nil t nil)))))
+                                    (candidates-in-buffer)
+                                    (type . file))))
+                          (defun helm-git-project-topdir ()
+                            (file-name-as-directory
+                             (replace-regexp-in-string
+                              "\n" ""
+                              (shell-command-to-string "git rev-parse --show-toplevel"))))
+                          (defun helm-git-project ()
+                            (interactive)
+                            (let ((topdir (helm-git-project-topdir)))
+                              (unless (file-directory-p topdir)
+                                (error "I'm not in Git Repository!!"))
+                              (let* ((default-directory topdir)
+                                     (sources (helm-c-sources-git-project-for default-directory)))
+                                (helm-other-buffer sources "*helm git project*"))))
+
+                          (global-set-key (kbd "C-q ;") 'helm-git-project)
 
 
                           ))
