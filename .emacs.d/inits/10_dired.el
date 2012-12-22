c65545f053b306801b7a050f45f05134b440221e
nishikawasasaki <kawaisosu.sosu@gmail.com>
2012-12-22 09:49:29 +0900

    add emacs settings     tramp-my-append-buffer-name-hint     my-append-buffer-name-hint

--- .emacs.d/inits/10_dired.el
+++ .emacs.d/inits/10_dired.el
@@ -216,3 +216,14 @@
 (define-key dired-mode-map (kbd "p") 'my/helm-dired)
 
 (define-key dired-mode-map (kbd "f") 'find-dired)
+
+;; Dired バッファに [Dired] を追加する
+(defun dired-my-append-buffer-name-hint ()
+  "Append a auxiliary string to a name of dired buffer."
+  (when (eq major-mode 'dired-mode)
+    (let* ((dir (expand-file-name list-buffers-directory))
+           (drive (if (and (eq 'system-type 'windows-nt) ;; Windows の場合はドライブレターを追加
+                           (string-match "^\\([a-zA-Z]:\\)/" dir))
+                      (match-string 1 dir) "")))
+      (rename-buffer (concat (buffer-name) " [" drive "Dired]") t))))
+(add-hook 'dired-mode-hook 'dired-my-append-buffer-name-hint)
