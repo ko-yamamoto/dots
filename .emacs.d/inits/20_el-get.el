a1793cf4f7c7626b1377b309889e0af844a8e47f
nishikawasasaki <kawaisosu.sosu@gmail.com>
2013-01-12 11:09:31 +0900

    add emacs elisp

--- .emacs.d/inits/20_el-get.el
+++ .emacs.d/inits/20_el-get.el
@@ -473,14 +473,14 @@
                           (back-button-mode 1)
                           ))
 
-          (:name mark-multiple.el-github
-                 :type github
-                 :url "git://github.com/magnars/mark-multiple.el.git"
-                 :after (progn
-                          (require 'mark-more-like-this)
-                          (global-set-key (kbd "C-<") 'mark-previous-like-this)
-                          (global-set-key (kbd "C->") 'mark-next-like-this)
-                          ))
+          ;; (:name mark-multiple.el-github
+          ;;        :type github
+          ;;        :url "git://github.com/magnars/mark-multiple.el.git"
+          ;;        :after (progn
+          ;;                 (require 'mark-more-like-this)
+          ;;                 (global-set-key (kbd "C-<") 'mark-previous-like-this)
+          ;;                 (global-set-key (kbd "C->") 'mark-next-like-this)
+          ;;                 ))
 
           (:name emacs-powerline-github
                  :type github
@@ -600,6 +600,31 @@
                                     (lambda ()
                                       (if (zerop (call-process-shell-command "git rev-parse --show-toplevel"))
                                           (git-gutter))))
+                          ))
+
+
+          (:name multiple-cursors-github
+                 :type github
+                 :url "git://github.com/magnars/multiple-cursors.el.git"
+                 :after (progn
+
+                          (require 'multiple-cursors)
+
+                          ;; 複数行選択してから全ての行にカーソル追加
+                          (global-set-key (kbd "C-q m m") 'mc/edit-lines)
+                          ;; リージョンと一致する箇所で現在行より下にあるもの1つを追加
+                          (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+                          ;; リージョンと一致する箇所で現在行より上にあるもの1つを追加
+                          (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+                          ;; リージョンと一致する箇所を↑↓キーで追加。←で追加せずに次の箇所へ。→で取り消し(1つ戻る)
+                          (global-set-key (kbd "C-q m e") 'mc/mark-more-like-this-extended)
+                          ;; リージョンと一致する箇所全て追加
+                          (global-set-key (kbd "C-q m a") 'mc/mark-all-like-this)
+                          ;; 良い感じに追加
+                          (global-set-key (kbd "C-q m d") 'mc/mark-all-like-this-dwim)
+                          ;; HTML などで対応するタグを追加
+                          (global-set-key (kbd "C-q m t") 'mc/mark-sgml-tag-pair)
+
                           ))
 
 
