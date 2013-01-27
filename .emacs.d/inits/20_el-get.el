ff95ae279466c029ba97590a4e7ae937a3822bd5
nishikawasasaki <kawaisosu.sosu@gmail.com>
2013-01-27 16:45:19 +0900

    add emacs setting     install guide-key

--- .emacs.d/inits/20_el-get.el
+++ .emacs.d/inits/20_el-get.el
@@ -640,6 +640,23 @@
 
                           ))
 
+          (:name guide-key-github
+                 :type github
+                 :url "git://github.com/kbkbkbkb1/guide-key.git"
+                 :after (progn
+
+                          (require 'guide-key)
+                          (setq guide-key/guide-key-sequence '("C-q" "C-c e" "C-c m" "C-c g" "C-q s" "C-c t" "C-x RET"))
+                          ;; (setq guide-key/highlight-command-regexp "rectangle")
+                          (guide-key-mode 1)  ; guide-key-mode を有効にする
+                          ))
+
+          (:name git-commit-mode-github
+                 :type github
+                 :url "git://github.com/rafl/git-commit-mode.git"
+                 )
+
+
 
 
           ))
