bd1fd9db1124cc8ef32ac38a13277606f488549f
nishikawasasaki <kawaisosu.sosu@gmail.com>
2013-01-27 16:41:26 +0900

    remove elisp

--- .emacs.d/inits/65_other-elisps.el
+++ .emacs.d/inits/65_other-elisps.el
@@ -216,24 +216,6 @@
 
 
 
-(when is_not_win
-  ;;====================
-  ;; emacs-evernote-mode
-  ;;====================
-  ;; (add-to-list 'load-path "<your load path>")
-  (require 'evernote-mode)
-  (setq evernote-username "momijishimeji") ; optional: you can use this username as default.
-  ;; (setq evernote-enml-formatter-command '("w3m" "-dump" "-I" "UTF8" "-O" "UTF8")) ; optional
-  (global-set-key "\C-cec" 'evernote-create-note)
-  (global-set-key "\C-ceo" 'evernote-open-note)
-  (global-set-key "\C-ces" 'evernote-search-notes)
-  (global-set-key "\C-ceS" 'evernote-do-saved-search)
-  (global-set-key "\C-cew" 'evernote-write-note)
-  (global-set-key "\C-cep" 'evernote-post-region)
-  (global-set-key "\C-ceb" 'evernote-browser))
-
-
-
 ;;====================
 ;; htmlize.el
 ;;====================
