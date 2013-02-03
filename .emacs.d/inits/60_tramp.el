f45f06a0e5957d7da2c65e51fc4c896b6db71ef0
nishikawasasaki <kawaisosu.sosu@gmail.com>
2013-02-03 21:55:06 +0900

    change emacs setting        update tramp from 2.2.4 to 2.2.6

--- .emacs.d/inits/60_tramp.el
+++ .emacs.d/inits/60_tramp.el
@@ -1,4 +1,4 @@
-(add-to-list 'load-path "~/.emacs.d/elisp/tramp-2.2.4/lisp/")
+;; (add-to-list 'load-path "~/.emacs.d/elisp/tramp-2.2.6/lisp/")
 (require 'tramp)
 
 (setq-default tramp-default-method "sshx")
