4cb0f899ae7852a191b6d12daaeb4e23f96724a6
nishikawasasaki <kawaisosu.sosu@gmail.com>
2013-02-03 22:36:43 +0900

    change emacs setting        new scala setting

--- .emacs.d/inits/40_scala-mode.el
+++ .emacs.d/inits/40_scala-mode.el
@@ -1,29 +1,19 @@
 ;;====================
 ;; scala-mode
 ;;====================
-(add-to-list 'load-path "~/.emacs.d/elisp/scala-mode")
-(require 'scala-mode-auto)
-(add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
 
-(require 'scala-mode-feature-electric)
-
-
-(add-hook 'scala-mode-hook
-          (lambda ()
-            (scala-electric-mode)
-            (define-key scala-mode-map my-shell-pop-key 'shell-pop)
-            ))
+;; see el-get
 
 ;; ensime
-;; (add-to-list 'load-path "~/.emacs.d/ensime/elisp/")
-;; (require 'ensime)
-;; (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+(add-to-list 'load-path "~/.emacs.d/elisp/ensime_2.10.0-RC3-0.9.8.2/elisp/")
+(require 'ensime)
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 
 
 ;; imenu用
 (add-hook 'scala-mode-hook
           (lambda ()
-            (scala-electric-mode)
+            ;; (scala-electric-mode) ;; not compatible with scala-mode2
             (setq imenu-generic-expression
                   '(
                     ("var" "\\(var +\\)\\([^(): ]+\\)" 2)
@@ -36,20 +26,3 @@
                     ("case class" "^[ \\t]*\\(case class +\\)\\([^(): ]+\\)" 2)
                     ("object" "\\(object +\\)\\([^(): ]+\\)" 2)
                     ))))
-
-
-;; ;; インデント修正
-(defadvice scala-block-indentation (around improve-indentation-after-brace activate)
-  (if (eq (char-before) ?\{)
-      (setq ad-return-value (+ (current-indentation) scala-mode-indent:step))
-    ad-do-it))
-(defun scala-newline-and-indent ()
-  (interactive)
-  (delete-horizontal-space)
-  (let ((last-command nil))
-    (newline-and-indent))
-  (when (scala-in-multi-line-comment-p)
-    (insert "* ")))
-(add-hook 'scala-mode-hook
-          (lambda ()
-            (define-key scala-mode-map (kbd "RET") 'scala-newline-and-indent)))
