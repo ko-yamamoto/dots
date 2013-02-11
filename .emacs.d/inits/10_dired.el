69fcd77a79cbcb4d1aa9345f5036be51b35d4d76
nishikawasasaki <kawaisosu.sosu@gmail.com>
2013-02-11 21:24:59 +0900

    add emacs setting     add dired-list-all-mode

--- .emacs.d/inits/10_dired.el
+++ .emacs.d/inits/10_dired.el
@@ -276,3 +276,46 @@
 
 ;; dired でのファイルサイズ表示のオプション
 (setq dired-listing-switches "-FlhA")
+
+
+
+
+;; diredの隠しファイル表示をトグルするminor-mode
+(defvar dired-list-all-switch "-A"
+  "Switch for listing dot files.
+Should be \"-a\" or \"-A\". Additional switch can be included.")
+
+(define-minor-mode dired-list-all-mode
+  "Toggle whether list dot files in dired.
+When using this mode the value of `dired-listing-switches' should not contain \"-a\" or \"-A\" option."
+  :init-value nil
+  :global nil
+  :lighter " ALL"
+  (when (eq major-mode 'dired-mode)
+    (dired-list-all-set)
+    (revert-buffer)))
+
+(defun dired-list-all-set ()
+  ""
+  (if dired-list-all-mode
+      (or (string-match-p dired-list-all-switch
+                          dired-actual-switches)
+          (setq dired-actual-switches
+                (concat dired-list-all-switch
+                        " "
+                        dired-actual-switches)))
+    (setq dired-actual-switches
+          (replace-regexp-in-string (concat dired-list-all-switch
+                                            " ")
+                                    ""
+                                    dired-actual-switches))))
+(add-hook 'dired-mode-hook
+          'dired-list-all-set)
+(provide 'dired-list-all-mode)
+;; a で dired の隠しファイル表示をトグル
+(when (require 'dired-list-all-mode nil t)
+  (setq dired-listing-switches "-lhFG")
+  (add-hook 'dired-mode-hook
+            (lambda ()
+              (define-key dired-mode-map "a" 'dired-list-all-mode)
+              )))
