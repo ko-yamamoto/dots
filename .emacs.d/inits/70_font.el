0a433d3a72fa44a711fcbda19611e9ca4a774637
nishikawasasaki <kawaisosu.sosu@gmail.com>
2013-02-22 11:58:47 +0900

    change emacs setting        font for linux

--- .emacs.d/inits/70_font.el
+++ .emacs.d/inits/70_font.el
@@ -9,20 +9,16 @@
 ;; | 数字 | アルファベット | 日本語     |
 ;; | 0123 | abcdefghijklmn | こんにちは |
 (when is_linux
-  ;; (set-default-font "VL ゴシック-10.0")
-  ;; (set-face-font 'variable-pitch "VL ゴシック-10.0")
-  ;; (set-fontset-font (frame-parameter nil 'font)
-  ;;                   'japanese-jisx0208
-  ;;                   '("VL ゴシック" . "unicode-bmp"))
-  ;; (set-default-font "ricty-9")
-  (set-default-font "ricty:pixelsize=12:spacing=0")
-  ;; (set-default-font "VL ゴシック-9")
-  ;; (set-face-font 'variable-pitch "ricty-9")
-  ;; (set-face-font 'variable-pitch "VL ゴシック-9")
+  (set-frame-font "M+ 1mn medium:pixelsize=12:spacing=0:slant=1")
+  (progn
+    (set-face-font 'default "M+ 1mn medium:pixelsize=12:spacing=0:slant=1")
+    (set-face-font 'bold "M+ 1mn bold:pixelsize=12:spacing=0:slant=1")
+    ;; (set-face-font 'italic "M+ 1mn bold:pixelsize=12:spacing=0:slant=1")
+    ;; (set-face-font 'bold-italic "M+ 1mn bold:pixelsize=12:spacing=0:slant=1")
+    )
   (set-fontset-font (frame-parameter nil 'font)
                     'japanese-jisx0208
-                    '("ricty" . "unicode-bmp"))
-                    ;; '("VL ゴシック" . "unicode-bmp"))
+                    '("M+ 1mn medium" . "unicode-bmp"))
 
 )
 (when is_win
@@ -32,7 +28,6 @@
                     'japanese-jisx0208
                     '("ricty" . "unicode-bmp"))
                     ;; '("MeiryoKe_Console" . "unicode-bmp"))
-
 )
 (when is_mac
   (set-frame-font "M+ 1mn medium:pixelsize=12:spacing=0:slant=1")
