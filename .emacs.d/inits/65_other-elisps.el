160bf526139de686349c541365e545e61707348e
nishikawasasaki <kawaisosu.sosu@gmail.com>
2013-02-27 12:41:43 +0900

    change emacs settings        remove emacs-minimap        move slime setting file        add dash from package.el

--- .emacs.d/inits/65_other-elisps.el
+++ .emacs.d/inits/65_other-elisps.el
@@ -50,38 +50,6 @@
 
 
 
-;;====================
-;; slime
-;;====================
-;; Clozure CLをデフォルトのCommon Lisp処理系に設定
-
-(when is_win
-  (setq inferior-lisp-program "ccl.bat"))
-(when is_mac
-  (setq inferior-lisp-program "ccl"))
-(when is_linux
-  (setq inferior-lisp-program "ccl"))
-
-
-;; ~/.emacs.d/slimeをload-pathに追加
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/slime"))
-;; SLIMEのロード
-(require 'slime)
-(slime-setup '(slime-repl slime-fancy slime-banner))
-;; SLIMEからの入力をUTF-8に設定
-(setq slime-net-coding-system 'utf-8-unix)
-
-;; slime キーバインドを設定
-(add-hook 'slime-mode-hook
-          '(lambda ()
-             (define-key slime-mode-map [(tab)]     'slime-indent-and-complete-symbol)))
-
-;; slime-repl再起動
-(add-hook 'slime-repl-mode-hook
-          '(lambda ()
-             (define-key slime-repl-mode-map "\C-c\M-r" 'slime-restart-inferior-lisp)))
-
-
 
 ;;====================
 ;; cyg-mount
@@ -177,7 +145,7 @@
 (autoload 'wipe "revive" "Wipe emacs" t)
 (add-hook 'kill-emacs-hook 'save-current-configuration)   ; 終了時に保存
 ;; 起動時に復元しないバッファ名を正規表現で指定
-(setq revive:ignore-buffer-pattern "\\.loaddefs\\.el")
+(setq revive:ignore-buffer-pattern "^\\( \\*\\)\\|\\(\\.emacs-histories\\)|\\(\\.loaddefs\\.el\\)")
 (resume) ; 起動時に復元
 
 ;; フレームの状態を保存
