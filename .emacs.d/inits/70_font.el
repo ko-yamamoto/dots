6659d39fd31cbdcb8357164f0c11ae6983ee692e
nishikawasasaki <kawaisosu.sosu@gmail.com>
2013-09-17 18:55:12 +0900

    change emacs setting        update font settings

--- .emacs.d/inits/70_font.el
+++ .emacs.d/inits/70_font.el
@@ -8,57 +8,41 @@
 ;; 下の縦棒が揃うこと
 ;; | 数字 | アルファベット | 日本語     |
 ;; | 0123 | abcdefghijklmn | こんにちは |
-(when is_linux
-  (set-frame-font "M+ 1mn medium:pixelsize=12:spacing=0:slant=1")
-  (progn
-    (set-face-font 'default "M+ 1mn medium:pixelsize=12:spacing=0:slant=1")
-    (set-face-font 'bold "M+ 1mn bold:pixelsize=12:spacing=0:slant=1")
-    ;; (set-face-font 'italic "M+ 1mn bold:pixelsize=12:spacing=0:slant=1")
-    ;; (set-face-font 'bold-italic "M+ 1mn bold:pixelsize=12:spacing=0:slant=1")
-    )
-  (set-fontset-font (frame-parameter nil 'font)
-                    'japanese-jisx0208
-                    '("M+ 1mn medium" . "unicode-bmp"))
 
-)
+(when is_linux
+
+  ;; バラバラに設定する場合
+  ;; 英字フォント
+  (set-face-attribute 'default nil
+                      :family "Hermit"
+                      :height 90)
+  ;; 漢字フォント
+  (set-fontset-font
+   nil 'japanese-jisx0208
+   (font-spec :family "ricty"))
+  ;; ひらがなかたかな
+  (set-fontset-font
+   nil '(#x3040 . #x30ff)
+   (font-spec :family "ricty"))
+
+  (setq face-font-rescale-alist
+        '((".Hermit.*" . 1.0)
+          (".ricty.*" . 1.2)
+          ("-cdac$" . 1.0)))
+
+  )
 (when is_win
   (set-default-font "ricty-10.5")
-  ;; (set-default-font "MeiryoKe_Console-10.5")
   (set-fontset-font (frame-parameter nil 'font)
                     'japanese-jisx0208
                     '("ricty" . "unicode-bmp"))
-                    ;; '("MeiryoKe_Console" . "unicode-bmp"))
-)
+  )
 (when is_mac
-  ;; (set-frame-font "M+ 1mn regular:pixelsize=12:spacing=0:slant=1")
   (set-frame-font "VL Gothic:pixelsize=12:spacing=0:slant=1")
   (progn
     (set-face-font 'default "VL Gothic:pixelsize=12:spacing=0:slant=1")
-    ;; (set-face-font 'default "M+ 1mn regular:pixelsize=12:spacing=0:slant=1")
-    ;; (set-face-font 'bold "M+ 1mn bold:pixelsize=12:spacing=0:slant=1")
-    ;; (set-face-font 'italic "M+ 1mn bold:pixelsize=12:spacing=0:slant=1")
-    ;; (set-face-font 'bold-italic "M+ 1mn bold:pixelsize=12:spacing=0:slant=1")
     )
   (set-fontset-font (frame-parameter nil 'font)
                     'japanese-jisx0208
                     '("VL Gothic" . "unicode-bmp"))
-
-  ;; バラバラに設定する場合
-  ;; ;; 英字フォントk
-  ;; (set-face-attribute 'default nil
-  ;;                     :family "ricty"
-  ;;                     :height 140)
-  ;; ;; 漢字フォント
-  ;; (set-fontset-font
-  ;;  nil 'japanese-jisx0208
-  ;;  (font-spec :family "NfMotoyaAporo"))
-  ;; ;; ひらがなかたかな
-  ;; (set-fontset-font
-  ;;  nil '(#x3040 . #x30ff)
-  ;;  (font-spec :family "NfMotoyaAporo"))
-
-  ;; (setq face-font-rescale-alist
-  ;;       '((".ricty.*" . 1.0)
-  ;;         (".NfMotoyaAporo.*" . 1.0)
-  ;;         ("-cdac$" . 1.0)))
-)
+  )
