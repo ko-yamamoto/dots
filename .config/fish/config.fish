a5f703bbb07d70846763a319a9a4d895cb58c465
nishikawasasaki <kawaisosu.sosu@gmail.com>
2013-10-29 23:45:01 +0900

    Mod fish setting     function cd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,10 +20,49 @@ set fish_greeting ""
 
 
 # cd したあとに ls
-function cd
-    builtin cd $argv
-    echo $PWD >> $CD_HISTORY_FILE # percol_cd_history 用
-    ls
+# function cd
+#     builtin cd $argv
+#     echo $PWD >> $CD_HISTORY_FILE # percol_cd_history 用
+#     ls
+# end
+function cd --description "Change directory"
+
+        # Skip history in subshells
+        if status --is-command-substitution
+                builtin cd $argv
+                  echo $PWD >> $CD_HISTORY_FILE # percol_cd_history 用
+                  ls
+                return $status
+        end
+
+        # Avoid set completions
+        set -l previous $PWD
+
+        if test $argv[1] = - ^/dev/null
+                if test "$__fish_cd_direction" = next ^/dev/null
+                        nextd
+                else
+                        prevd
+                end
+
+                echo $PWD >> $CD_HISTORY_FILE # percol_cd_history 用
+                ls
+                return $status
+        end
+
+        builtin cd $argv[1]
+        set -l cd_status $status
+
+        if test $cd_status = 0 -a "$PWD" != "$previous"
+                set -g dirprev $dirprev $previous
+                set -e dirnext
+                set -g __fish_cd_direction prev
+        end
+
+        echo $PWD >> $CD_HISTORY_FILE # percol_cd_history 用
+        ls
+
+        return $cd_status
 end
 
 # emacsclient を ec で
