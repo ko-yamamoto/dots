e1256f2bd3c629ddced43e43ba866de00945df48
nishikawasasaki <kawaisosu.sosu@gmail.com>
2013-11-22 20:03:29 +0900

    Changed emacs setting         use Yasashisa Gothic font

--- .emacs.d/inits/70_font.el
+++ .emacs.d/inits/70_font.el
@@ -19,15 +19,18 @@
   ;; 漢字フォント
   (set-fontset-font
    nil 'japanese-jisx0208
-   (font-spec :family "ricty"))
+   ;; (font-spec :family "ricty"))
+   (font-spec :family "07YasashisaGothic"))
   ;; ひらがなかたかな
   (set-fontset-font
    nil '(#x3040 . #x30ff)
-   (font-spec :family "ricty"))
+   ;; (font-spec :family "ricty"))
+   (font-spec :family "07YasashisaGothic"))
 
   (setq face-font-rescale-alist
-        '((".Hermit.*" . 1.0)
-          (".ricty.*" . 1.2)
+        '((".*Hermit.*" . 1.0)
+          (".*ricty.*" . 1.2)
+          (".*やさしさ.*" . 1.2)
           ("-cdac$" . 1.0)))
 
   )
