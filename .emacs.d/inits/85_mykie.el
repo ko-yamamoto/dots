f59aaae08b5876149adbacf9dd54d40f459c6d91
nishikawasasaki <kawaisosu.sosu@gmail.com>
2014-01-15 17:14:55 +0900

    Added emacs setting         Added mykie keybind

--- .emacs.d/inits/85_mykie.el
+++ .emacs.d/inits/85_mykie.el
@@ -12,12 +12,40 @@
                 '(lambda ()
                    (interactive)
                    (mykie
-                    :default            '(progn
-                                           ;; 無駄なスペースを削除
-                                           (delete-trailing-whitespace)
-                                           (case major-mode
-                                             (org-mode (org-return-indent))
-                                             (t        (newline-and-indent))))
+                    :default  '(progn
+                                 ;; 無駄なスペースを削除
+                                 (delete-trailing-whitespace)
+                                 (case major-mode
+                                   (org-mode (org-return-indent))
+                                   (t        (newline-and-indent))))
                     ;; 行末で C-u+C-j で fill-region を実行する
-                    :C-u&eolp           '(fill-region (point-at-bol) (point-at-eol))
-                    :region             'query-replace-regexp)))
+                    :C-u&eolp '(fill-region (point-at-bol) (point-at-eol))
+                    :region   'query-replace-regexp)))
+
+(global-set-key (kbd "C-;")
+                '(lambda ()
+                   (interactive)
+                   (mykie
+                    :default            'helm-my
+                    :region             'helm-occur
+                    :region-handle-flag 'copy)))
+
+(global-set-key (kbd "o")
+                '(lambda ()
+                   (interactive)
+                   (mykie
+                    :default 'self-insert-command
+                    :region 'occur-by-moccur
+                    :region&C-u 'moccur-grep-find
+                    :region-handle-flag 'copy)))
+
+(global-set-key (kbd "r")
+                '(lambda ()
+                   (interactive)
+                   (mykie
+                    :default 'self-insert-command
+                    :region '(progn
+                               (deactivate-mark)
+                               (anzu-query-replace))
+                    :region&C-u 'anzu-query-replace
+                    :region-handle-flag 'copy)))
