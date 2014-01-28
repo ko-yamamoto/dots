dd4076d40fa31d7904ae0978b7c0ee9491e088ad
nishikawasasaki <kawaisosu.sosu@gmail.com>
2014-01-28 19:20:34 +0900

    Changed emacs setting         update mykie-el setting for new version

--- .emacs.d/inits/85_mykie.el
+++ .emacs.d/inits/85_mykie.el
@@ -1,51 +1,40 @@
 (require 'mykie)
+(setq mykie:use-major-mode-key-override t)
+(mykie:initialize)
 
+(mykie:global-set-key "C-a"
+  :default     my-toggle-beginning-of-line-and-sentence
+nnnnnnnnnnnn  :C-u         mark-whole-buffer)
 
-(global-set-key (kbd "C-a")
-                '(lambda ()
-                   (interactive)
-                   (mykie
-                    :default     'my-toggle-beginning-of-line-and-sentence
-                    :C-u         'mark-whole-buffer)))
+(mykie:global-set-key "C-j"
+  :default  (progn
+              ;; 無駄なスペースを削除
+              (delete-trailing-whitespace)
+              (case major-mode
+                (org-mode (org-return-indent))
+                (t        (newline-and-indent))))
+  ;; 行末で C-u+C-j で fill-region を実行する
+  :C-u&eolp (fill-region (point-at-bol) (point-at-eol))
+  :region   query-replace-regexp)
 
-(global-set-key (kbd "C-j")
-                '(lambda ()
-                   (interactive)
-                   (mykie
-                    :default  '(progn
-                                 ;; 無駄なスペースを削除
-                                 (delete-trailing-whitespace)
-                                 (case major-mode
-                                   (org-mode (org-return-indent))
-                                   (t        (newline-and-indent))))
-                    ;; 行末で C-u+C-j で fill-region を実行する
-                    :C-u&eolp '(fill-region (point-at-bol) (point-at-eol))
-                    :region   'query-replace-regexp)))
+(mykie:global-set-key "C-;"
+  :default            helm-my
+  :region             helm-occur
+  :region-handle-flag copy)
 
-(global-set-key (kbd "C-;")
-                '(lambda ()
-                   (interactive)
-                   (mykie
-                    :default            'helm-my
-                    :region             'helm-occur
-                    :region-handle-flag 'copy)))
+(mykie:global-set-key "o"
+  :default self-insert-command
+  :region occur-by-moccur
+  :region&C-u moccur-grep-find
+  :region-handle-flag copy)
 
-(global-set-key (kbd "o")
-                '(lambda ()
-                   (interactive)
-                   (mykie
-                    :default 'self-insert-command
-                    :region 'occur-by-moccur
-                    :region&C-u 'moccur-grep-find
-                    :region-handle-flag 'copy)))
-
-(global-set-key (kbd "r")
-                '(lambda ()
-                   (interactive)
-                   (mykie
-                    :default 'self-insert-command
-                    :region '(progn
-                               (deactivate-mark)
-                               (anzu-query-replace))
-                    :region&C-u 'anzu-query-replace
-                    :region-handle-flag 'copy)))
+;; (mykie:global-set-key "r"
+;;   :default self-insert-command
+;;   :region (progn
+;;             (beginning-of-buffer)
+;;             ;; (deactivate-mark)
+;;             ;; (anzu-query-replace (substring-no-properties (car kill-ring)))
+;;             (anzu-query-replace)
+;;             )
+;;   :region&C-u anzu-query-replace
+;;   :region-handle-flag copy)
