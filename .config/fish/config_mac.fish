d73a3def85c2a7ec24140f4900b09ecb9215a77f
nishikawasasaki <kawaisosu.sosu@gmail.com>
2014-02-01 16:44:52 +0900

    Fixed fish shell setting       ssh-agent problem

--- .config/fish/config_mac.fish
+++ .config/fish/config_mac.fish
@@ -10,3 +10,19 @@ end
 function ec
     emacsclient -n $argv
 end
+
+# ssh-agent
+set agentPID (ps gxww | grep "ssh-agent]*\$" | awk '{print $1}') | head -1
+set agentSOCK (/bin/ls -t /private/var/folders/fw/_tk7tz0x0g95c6jp7gw2j4hr0000gn/T/ssh*/agent*|head -1)
+if test "$agentPID" = "" -o "$agentSOCK" = ""
+    set -e SSH_AUTH_SOCK
+    set -e SSH_AGENT_PID
+    eval (ssh-agent)
+    ssh-add
+else
+    set --export SSH_AGENT_PID $agentPID
+    set --export SSH_AUTH_SOCK $agentSOCK
+    # if [ `ssh-add -l` = "" ]; then
+    #     ssh-add < /dev/null
+    # fi
+end
