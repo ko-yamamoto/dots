3ebf05a63a7d4faa75e690c124f9888d284bfb3b
nishikawasasaki <kawaisosu.sosu@gmail.com>
2014-03-21 15:03:23 +0900

    Fixed emacs setting       TAB visible setting bug

--- .emacs.d/inits/70_visual.el
+++ .emacs.d/inits/70_visual.el
@@ -29,17 +29,18 @@
 
 ;; タブ, 全角スペース, 行末空白表示
 (defface my-face-b-1 '((t (:background "gray80"))) nil) ; 全角スペース
-;; (defface my-face-b-2 '((t (:background "gray80"))) nil) ; タブ
-;;(defface my-face-u-1 '((t (:background "SteelBlue" :underline t))) nil) ; 行末空白
+(defface my-face-b-2 '((t (:background "gray80"))) nil) ; タブ
+(defface my-face-u-1 '((t (:background "SteelBlue" :underline t))) nil) ; 行末空白
 (defface my-face-u-1 '((t (:background "gray80"))) nil) ; 行末空白
 (defvar my-face-b-1 'my-face-b-1)
-;; (defvar my-face-b-2 'my-face-b-2)
+(defvar my-face-b-2 'my-face-b-2)
 (defvar my-face-u-1 'my-face-u-1)
 
 (defadvice font-lock-mode (before my-font-lock-mode ())
   (font-lock-add-keywords
    major-mode
-   '(("\t" 0 my-face-b-2 append)
+   '(
+     ;; ("\t" 0 my-face-b-2 append)
      ("　" 0 my-face-b-1 append)
      ("[ \t]+$" 0 my-face-u-1 append)
      )))
