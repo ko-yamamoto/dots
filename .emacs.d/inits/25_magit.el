6cd5947d25f10449c9cc07265e634631b94c2a12
nishikawasasaki <kawaisosu.sosu@gmail.com>
2015-01-12 18:01:02 +0900

    Chande magit settings for use-package

--- .emacs.d/inits/25_magit.el
+++ .emacs.d/inits/25_magit.el
@@ -1,23 +1,21 @@
 (use-package magit
+  :defer t
   :ensure t
   :bind (("C-c g g" . magit-status)
          ("C-c g d" . magit-diff-working-tree)
          ("C-c g f" . magit-file-log))
   :config
-
   (require 'vc)
 
   (defun my/git-commit-commit ()
     (interactive)
     (git-commit-commit)
     (elscreen-kill))
-  (define-key git-commit-mode-map (kbd "C-c C-c") 'my/git-commit-commit)
 
   (defun my/magit-quit-session ()
     (interactive)
     (magit-mode-quit-window)
     (delete-window))
-  (define-key magit-status-mode-map (kbd "q") 'my/magit-quit-session)
 
   ;; 色変更
   ;; (set-face-foreground 'magit-diff-add "#b9ca4a")
@@ -25,4 +23,11 @@
   ;; (set-face-foreground 'magit-diff-del "#d54e53")
   ;; (set-face-background 'magit-diff-del "#000000")
   ;; (set-face-background 'magit-item-highlight "#000000")
+
+    (bind-keys :map git-commit-mode-map
+               ("C-c C-c" . my/git-commit-commit))
+
+    (bind-keys :map magit-status-mode-map
+               ("q" . my/magit-quit-session))
+
   )
