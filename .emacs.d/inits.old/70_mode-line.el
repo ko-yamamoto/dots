75f18a97e8adb02c99e5a270c5f003971c794e1e
nishikawasasaki <kawaisosu.sosu@gmail.com>
2015-01-12 18:02:32 +0900

    Renamed old emacs settings dir

---
+++ .emacs.d/inits.old/70_mode-line.el
@@ -0,0 +1,63 @@
+;; モードライン (mode-line-format)での書式記号
+;; %b -- print buffer name.
+;; %f -- print visited file name.
+;; %F -- print frame name.
+;; %* -- print %, * or hyphen.
+;; %+ -- print *, % or hyphen.
+;;       %& is like %*, but ignore read-only-ness.
+;;       % means buffer is read-only and * means it is modified.
+;;       For a modified read-only buffer, %* gives % and %+ gives *.
+;; %s -- print process status.   %l -- print the current line number.
+;; %c -- print the current column number (this makes editing slower).
+;;       To make the column number update correctly in all cases,`column-number-mode' must be non-nil.
+;; %i -- print the size of the buffer.
+;; %I -- like %i, but use k, M, G, etc., to abbreviate.
+;; %p -- print percent of buffer above top of window, or Top, Bot or All.
+;; %P -- print percent of buffer above bottom of window, perhaps plus Top, or print Bottom or All.
+;; %n -- print Narrow if appropriate.
+;; %t -- visited file is text or binary (if OS supports this distinction).
+;; %z -- print mnemonics of keyboard, terminal, and buffer coding systems.
+;; %Z -- like %z, but including the end-of-line format.
+;; %e -- print error message about full memory.
+;; %@ -- print @ or hyphen.  @ means that default-directory is on a remote machine.
+;; %[ -- print one [ for each recursive editing level.  %] similar.
+;; %% -- print %.
+;; %- -- print infinitely many dashes.
+
+;; モードライン
+(setq-default mode-line-format
+              (list "%*[" 'mode-line-mule-info "] L%l:C%c %P [" `(vc-mode vc-mode) "]   %f   (%m" 'minor-mode-alist ")"))
+
+;; -> el-get の powerline で設定
+
+;; モード名をエイリアス
+(defvar mode-line-cleaner-alist
+  '( ;; For minor-mode, first char is 'space'
+    (yas-minor-mode . " Yas")
+    (abbrev-mode . "")
+    (guide-key-mode . "")
+    (undo-tree-mode . "")
+    (wrap-region-mode . "")
+    (smooth-scroll-mode . "")
+    (helm-mode . "")
+    (back-button-mode . "")
+    ;; Major modes
+    (lisp-interaction-mode . "Li")
+    (python-mode . "Py")
+    (ruby-mode   . "Rb")
+    (emacs-lisp-mode . "El")
+    (lisp-mode . "Li")
+    (markdown-mode . "Md")))
+
+(defun clean-mode-line ()
+  (interactive)
+  (loop for (mode . mode-str) in mode-line-cleaner-alist
+        do
+        (let ((old-mode-str (cdr (assq mode minor-mode-alist))))
+          (when old-mode-str
+            (setcar old-mode-str mode-str))
+          ;; major mode
+          (when (eq mode major-mode)
+            (setq mode-name mode-str)))))
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
