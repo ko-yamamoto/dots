07a70e84078312e457c8b11cb2e527a451671135
nishikawasasaki <kawaisosu.sosu@gmail.com>
2015-01-19 08:19:25 +0900

    Added emacs scala-mode

---
+++ .emacs.d/inits/50_scala-mode.el
@@ -0,0 +1,20 @@
+(use-package scala-mode2
+  :ensure t
+  :defer t
+  ;; :config
+  ;; コメントスタイルは javadoc 風に
+  ; (set scala-indent:use-javadoc-style t)
+
+;;   ;; imenu
+;;   (setq imenu-generic-expression
+;;         '(
+;;           ("var" "\\(var +\\)\\([^(): ]+\\)" 2)
+;;           ("val" "\\(val +\\)\\([^(): ]+\\)" 2)
+;;           ("override def" "^[ \\t]*\\(override\\) +\\(def +\\)\\([^(): ]+\\)" 3)
+;;           ("implicit def" "^[ \\t]*\\(implicit\\) +\\(def +\\)\\([^(): ]+\\)" 3)
+;;           ("def" "^[ \\t]*\\(def +\\)\\([^(): ]+\\)" 2)
+;;           ("trait" "\\(trait +\\)\\([^(): ]+\\)" 2)
+;;           ("class" "^[ \\t]*\\(class +\\)\\([^(): ]+\\)" 2)
+;;           ("case class" "^[ \\t]*\\(case class +\\)\\([^(): ]+\\)" 2)
+;;           ("object" "\\(object +\\)\\([^(): ]+\\)" 2)))
+  )
