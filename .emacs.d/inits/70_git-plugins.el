27cc7d385c9658f8174b764f8b968ff946d27213
nishikawasasaki <kawaisosu.sosu@gmail.com>
2015-01-19 20:01:27 +0900

    Fixed emacs git-gutter+ mode setting

--- .emacs.d/inits/70_git-plugins.el
+++ .emacs.d/inits/70_git-plugins.el
@@ -24,11 +24,11 @@
   ;; (set-face-background 'magit-diff-del "#000000")
   ;; (set-face-background 'magit-item-highlight "#000000")
 
-    (bind-keys :map git-commit-mode-map
-               ("C-c C-c" . my/git-commit-commit))
+  (bind-keys :map git-commit-mode-map
+             ("C-c C-c" . my/git-commit-commit))
 
-    (bind-keys :map magit-status-mode-map
-               ("q" . my/magit-quit-session))
+  (bind-keys :map magit-status-mode-map
+             ("q" . my/magit-quit-session))
 
   )
 
@@ -42,22 +42,22 @@
   :ensure t
   :bind (("C-c g c" . git-gutter:toggle))
   :config
-  (global-git-gutter+-mode t) ;; el-get が誤作動するため global は使わない
-  ;; ;; 指定したモードで有効に
-  ;; (let ((mode-hooks
-  ;;        '(emacs-lisp-mode-hook
-  ;;          org-mode-hook
-  ;;          rst-mode-hook
-  ;;          java-mode-hook
-  ;;          lisp-mode-hook
-  ;;          clojure-mode-hook
-  ;;          scala-mode-hook
-  ;;          haskell-mode-hook
-  ;;          sh-mode-hook
-  ;;          go-mode-hook
-  ;;          python-mode-hook
-  ;;          ruby-mode-hook)))
-  ;;   (mapcar (lambda (mode-hook) (add-hook mode-hook 'git-gutter-mode)) mode-hooks))
+  ;; (global-git-gutter+-mode t)
+  ;; 指定したモードで有効に
+  (let ((mode-hooks
+         '(emacs-lisp-mode-hook
+           org-mode-hook
+           rst-mode-hook
+           java-mode-hook
+           lisp-mode-hook
+           clojure-mode-hook
+           scala-mode-hook
+           haskell-mode-hook
+           sh-mode-hook
+           go-mode-hook
+           python-mode-hook
+           ruby-mode-hook)))
+    (mapcar (lambda (mode-hook) (add-hook mode-hook 'git-gutter+-mode)) mode-hooks))
 
   ;; 表示変更
   (setq git-gutter+-window-width 1)
@@ -82,91 +82,91 @@
   (setq-default right-fringe-width 10)
   ;; 形変更 (上下くっつけるため目一杯長く)
   (fringe-helper-define 'git-gutter-fr+-added nil
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX")
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX")
   (fringe-helper-define 'git-gutter-fr+-deleted nil
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX")
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX")
   (fringe-helper-define 'git-gutter-fr+-modified nil
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX"
-                        "XXXXXXXXX")
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX"
+    "XXXXXXXXX")
 
   )
