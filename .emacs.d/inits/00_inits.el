817c6f66f66548b5a6c437f649ec73f45e0fe859
nishikawasasaki <kawaisosu.sosu@gmail.com>
2015-01-20 23:28:46 +0900

    Moved key-chord setting to 10_general.el

--- .emacs.d/inits/00_inits.el
+++ .emacs.d/inits/00_inits.el
@@ -56,25 +56,3 @@
 (unless (require 'use-package nil t)
   (message "Use-package is unavailable!")
   (defmacro use-package (&rest _args)))
-
-
-
-;; Keycord ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(use-package key-chord
-  :ensure t
-  ;; :defer t
-  :config
-  (key-chord-mode 1)
-  (setq key-chord-one-keys-delay 0.04)
-
-  ;; don't hijack input method!
-  (defadvice toggle-input-method (around toggle-input-method-around activate)
-    (let ((input-method-function-save input-method-function))
-      ad-do-it
-      (setq input-method-function input-method-function-save)))
-  (key-chord-define-global "mk" 'kill-buffer)
-  (key-chord-define-global "MK" 'my/buffer-kill-and-delete-window)
-
-  (key-chord-define-global "cl" 'toggle-truncate-lines)
-
-  )
