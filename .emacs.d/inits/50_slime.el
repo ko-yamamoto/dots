90bf3d2a1fd74a706f01591eda2cc18914276f53
nishikawasasaki <kawaisosu.sosu@gmail.com>
2015-03-03 23:51:31 +0900

    Add emacs slime setting

---
+++ .emacs.d/inits/50_slime.el
@@ -0,0 +1,21 @@
+(use-package slime
+  :ensure t
+  :defer t
+  :config
+  ;; Clozure CLをデフォルトのCommon Lisp処理系に設定
+  (when is_win
+    (setq inferior-lisp-program "ccl.bat"))
+  (when is_mac
+    (setq inferior-lisp-program "ccl"))
+  (when is_linux
+    (setq inferior-lisp-program "ccl"))
+
+  (require 'slime-autoloads)
+  (slime-setup '(slime-repl))
+
+  ;; (require 'ac-slime)
+  ;; (add-hook 'slime-mode-hook 'set-up-slime-ac)
+  ;; (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+  ;; (eval-after-load "auto-complete"
+  ;;   '(add-to-list 'ac-modes 'slime-repl-mode))
+  )
