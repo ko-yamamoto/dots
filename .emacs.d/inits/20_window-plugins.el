2e81d200291ea565eadbc15130a24752d8476fed
nishikawasasaki <kawaisosu.sosu@gmail.com>
2015-09-06 09:49:41 +0900

    Remove unused emacs plugin

--- .emacs.d/inits/20_window-plugins.el
+++ .emacs.d/inits/20_window-plugins.el
@@ -96,53 +96,6 @@
   ;; (global-set-key (kbd "C-g") 'winner-undo)
   )
 
-(use-package popwin
-  :disabled t
-  :ensure t
-  ;; :defer t
-  :config
-  (setq display-buffer-function 'popwin:display-buffer)
-
-  ;; popwinのデフォルトサイズ
-  (setq popwin:popup-window-height 0.4)
-  ;; popwinを使う表示を設定
-  (setq popwin:special-display-config
-        (append '(("*Remember*" :stick t)
-                  ("*Backtrace*")
-                  ("*Messages*")
-                  ("*Completion*" :height 0.2)
-                  ("*Compile-Log*")
-                  ("*compilation*") ; for rst-compile
-                  ("*sdic*" :noselect t)
-                  ("*helm \\(.*\\)*" :regexp t :height 0.35 )
-                  ;; ("*helm my*" :position bottom)
-                  ;; ("\\(.*\\) \\[Dired\\]" :regexp t :height 0.4 :position top :stick t)
-                  ;;("*Moccur*" :height 20)
-                  ("*Directory*" :height 20)
-                  ("*magit:\\(.*\\)*" :regexp t :height 0.65 :dedicated t)
-                  ("COMMIT_EDITMSG" :dedicated t)
-                  ;; slime
-                  ("*slime-apropos*")
-                  ("*slime-macroexpansion*")
-                  ("*slime-description*")
-                  ("*slime-compilation*" :noselect t)
-                  ("*slime-xref*")
-                  ("*slime-repl clojure*" :height 30)
-                  ("*slime-repl ccl*" :height 30)
-                  ("\\*sldb clojure*" :regexp t :height 30)
-                  (sldb-mode :stick t)
-                  (slime-repl-mode)
-                  (slime-connection-list-mode)
-                  (direx:direx-mode :position left :width 0.3)
-                  ("*auto-async-byte-compile*" :height 20)
-                  ("*Shell Command Output*" :height 25)
-                  ("*svn output*" :stick t :position right)
-                  ("\\*nrepl-*" :height 0.3 :regexp t)
-                  )
-                popwin:special-display-config))
-  )
-
-
 (use-package scratch-log
   :ensure t
   :config
