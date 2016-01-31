eb735a7d961ec7476520449a2bc279138e56e890
nishikawasasaki <kawaisosu.sosu@gmail.com>
2016-01-31 18:56:35 +0900

    Add emacs function

--- .emacs.d/inits/20_window-plugins.el
+++ .emacs.d/inits/20_window-plugins.el
@@ -52,6 +52,18 @@
     (let ((current-dir (expand-file-name ".")))
       (elscreen-create)
       (dired current-dir)))
+
+  (defun temp-display-tab ()
+    ;; 一定時間タブを表示
+    (setq elscreen-display-tab t)
+    (run-at-time "1.5 sec" nil
+                 #'(lambda ()
+                     (setq elscreen-display-tab nil)
+                     (elscreen-notify-screen-modification 'force))))
+
+  (advice-add 'elscreen-next :after #'temp-display-tab)
+  (advice-add 'elscreen-previous :after #'temp-display-tab)
+
   )
 
 (use-package elscreen-persist
