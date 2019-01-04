da64af2c391521d4247fa7f1f31c3b66e4aa3b5d
nishikawasasaki <kawaisosu.sosu@gmail.com>
2019-01-04 10:08:59 +0900

    dired にアイコン表示関連の設定を追加

---
+++ .emacs.d/inits/30_check-plugin.el
@@ -0,0 +1,20 @@
+(use-package flycheck
+  :ensure t
+  ;; :init (global-flycheck-mode)
+  :config
+
+  ;; textlint
+  (flycheck-define-checker textlint
+    "A linter for Markdown."
+    :command ("textlint" "--format" "unix" source)
+    :error-patterns
+    ((warning line-start (file-name) ":" line ":" column ": "
+              (id (one-or-more (not (any " "))))
+              (message (one-or-more not-newline)
+                       (zero-or-more "\n" (any " ") (one-or-more not-newline)))
+              line-end))
+    :modes (text-mode markdown-mode gfm-mode))
+  (add-hook 'markdown-mode-hook
+            '(lambda ()
+               (setq flycheck-checker 'textlint)
+               (flycheck-mode 1))))
