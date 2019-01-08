87283e669fdcb305c85cb7f696f5b13449625d93
nishikawasasaki <kawaisosu.sosu@gmail.com>
2019-01-08 19:04:24 +0900

    Change fish settings for 3.0

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,78 @@
-set --export LANG "ja_JP.UTF-8"
+# set -x LANG "ja_JP.UTF-8"
+set -x LANG "en_US.utf8"
 
-# PATH
-set --export GOPATH $HOME/go
-set PATH $HOME/.autojump/bin $GOPATH/bin $HOME/.cabal/bin $HOME/.rbenv/shims $HOME/bin $HOME/.rbenv/bin /usr/bin /opt/bin $PATH
+set PATH $HOME/bin $PATH
+
+set -x DISPLAY localhost:0.0
+
+function peco_select_history
+    if set -q $argv
+        history | peco | read -l line && commandline $line
+    else
+        history | peco --query $argv | read -l line && commandline $line
+    end
+
+    commandline -f repaint
+end
+
+#peco
+function fish_user_key_bindings
+    bind \cr peco_select_history
+    end
+
+
+# ssh-agent
+set agentPID (ps gxww | grep "ssh-agent]*\$" | awk '{print $1}') | head -1
+set agentSOCK (/bin/ls -t /tmp/ssh*/agent*|head -1)
+if test "$agentPID" = "" -o "$agentSOCK" = ""
+    set -e SSH_AUTH_SOCK
+    set -e SSH_AGENT_PID
+    eval (ssh-agent)
+    ssh-add
+else
+    set --export SSH_AGENT_PID $agentPID
+    set --export SSH_AUTH_SOCK $agentSOCK
+    # if [ `ssh-add -l` = "" ]; then    #     ssh-add < /dev/null
+    # fi
+end
+
+
+function cd
+    if test (count $argv) -eq 0
+        cd $HOME
+        return 0
+    else if test (count $argv) -gt 1
+        printf "%s\n" (_ "Too many args for cd command")
+        return 1
+    end
+    # Avoid set completions.
+    set -l previous $PWD
+
+    if test "$argv" = "-"
+        if test "$__fish_cd_direction" = "next"
+            nextd
+        else
+            prevd
+        end
+        return $status
+    end
+    builtin cd $argv
+    set -l cd_status $status
+    # Log history
+    if test $cd_status -eq 0 -a "$PWD" != "$previous"
+        set -q dirprev[$MAX_DIR_HIST]
+        and set -e dirprev[1]
+        set -g dirprev $dirprev $previous
+        set -e dirnext
+        set -g __fish_cd_direction prev
+    end
+
+    if test $cd_status -ne 0
+        return 1
+    end
+    ls
+    return $status
+end
 
 
 # alias や function で作った別名コマンドに補完を適用する関数
@@ -20,75 +90,6 @@ end
 # ログインメッセージ無効
 set fish_greeting ""
 
-# cd したあとに ls
-# function cd
-#     builtin cd $argv
-#     echo $PWD >> $CD_HISTORY_FILE # percol_cd_history 用
-#     ls
-# end
-function cd --description "Change directory"
-
-        # Skip history in subshells
-        if status --is-command-substitution
-                builtin cd $argv
-                  echo $PWD >> $CD_HISTORY_FILE # percol_cd_history 用
-                  ls
-                return $status
-        end
-
-        # Avoid set completions
-        set -l previous $PWD
-
-        if test $argv[1] = - ^/dev/null
-                if test "$__fish_cd_direction" = next ^/dev/null
-                        nextd
-                else
-                        prevd
-                end
-
-                echo $PWD >> $CD_HISTORY_FILE # percol_cd_history 用
-                ls
-                return $status
-        end
-
-        builtin cd $argv[1]
-        set -l cd_status $status
-
-        if test $cd_status = 0 -a "$PWD" != "$previous"
-                set -g dirprev $dirprev $previous
-                set -e dirnext
-                set -g __fish_cd_direction prev
-        end
-
-        echo $PWD >> $CD_HISTORY_FILE # percol_cd_history 用
-        ls
-
-        return $cd_status
-end
-
-
-# プロンプトの pwd 表示を変更
-if test (uname) = Darwin
-        function prompt_pwd --description "Print the current working directory, shortend to fit the prompt"
-                echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||' -e 's-\([^/]\{,5\}\)[^/]*/-\1\.\./-g'
-        end
-else
-        function prompt_pwd --description "Print the current working directory, shortend to fit the prompt"
-                echo $PWD | sed -e "s|^$HOME|~|" -e 's-\([^/]\{,5\}\)[^/]*/-\1\.\./-g'
-        end
-end
-
-
-# top の代わりに htop する
-function top
-    htop $args
-end
-
-
-# emacsclient を ec で
-function ec
-    emacsclient -n $argv
-end
 
 # git を g で
 function g
@@ -98,77 +99,13 @@ end
 make_completion g 'git'
 
 
-
-
-##
-# Read Z extension
-##
-if test -f $HOME/bin/z-fish/z.fish
-  . $HOME/bin/z-fish/z.fish
+function runemacs
+    nohup emacs26 &
+end
+function ec
+    emacsclient26 -n $argv
 end
 
-#
-# for autojump
-#
-function j
-   cd (command autojump $argv)
-end
-
-# percol を使った history 補完
-function percol_select_history
-  history|percol|read percolhistry
-  if [ $percolhistry ]
-    commandline $percolhistry
-  else
-    commandline ''
-  end
-end
-
-
-set CD_HISTORY_FILE $HOME/.cd_history_file # cd 履歴の記録先ファイル
-# percol を使って cd 履歴の中からディレクトリを選択
-# 過去の訪問回数が多いほど選択候補の上に来る
-function percol_cd_history
-  sort $CD_HISTORY_FILE | uniq -c | sort -r | sed -e 's/^[ ]*[0-9]*[ ]*//' | percol | read -l percolCDhistory
-  if [ $percolCDhistory ]
-    # commandline 'cd '
-    # commandline -i $percolCDhistory
-    echo 'cd' $percolCDhistory
-    cd $percolCDhistory
-    echo $percolCDhistory
-    commandline -f repaint
-  else
-    commandline ''
-  end
-end
-
-
-# キーバインドの追加 ############################################
-function fish_user_key_bindings
-  bind \cg 'commandline ""' # C-g で C-c する
-  bind \cr percol_select_history
-  bind \cx percol_cd_history
-end
-
-
-# 環境毎の設定読み込み ##########################################
-switch (uname)
-  case Darwin
-    . $HOME/.config/fish/config_mac.fish
-  case Linux
-    . $HOME/.config/fish/config_gentoo.fish
-end
-
-
-
-
-
-
-set fish_color_search_match --background=white
-set fish_color_command blue
-set fish_color_error red
-set fish_color_autosuggestion yellow
-set fish_color_param green
 
 # other-writable(777) なものは赤い下線
 set --export LS_COLORS "ow=04;31:tw=04;31"
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,42 +1,47 @@
-function fish_prompt --description 'Write out the prompt'
-	set -l last_status $status
+set fish_color_search_match --background=blue
+set fish_color_command green
+set fish_color_error red
+set fish_color_autosuggestion brblack
+set fish_color_param white
 
-  # User
-  set_color $fish_color_user
-  echo -n (whoami)
-  set_color normal
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch blue
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
 
-  echo -n '@'
+# Status Chars
+set __fish_git_prompt_char_dirtystate '!'
+set __fish_git_prompt_char_stagedstate 's'
+set __fish_git_prompt_char_untrackedfiles '?'
+set __fish_git_prompt_char_stashstate ''
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
 
-  # Host
-  set_color $fish_color_host
-  # echo -n (hostname -s)
-  echo -n (hostname)
-  set_color normal
 
-  echo -n ':'
+function fish_prompt
 
-  # PWD
-  set_color $fish_color_cwd
-  echo -n (prompt_pwd)
-  set_color normal
+    function prompt_pwd
+    # ディレクトリ名の省略をしないように関数を上書き
+      echo $PWD | sed -e "s|^$HOME|~|"
+    end
 
-  __terlar_git_prompt
-  echo
+	test $SSH_TTY
+    and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
+    test "$USER" = 'root'
+    and echo (set_color red)"#"
 
-  switch (echo $last_status)
-	case 0
-		set_color $fish_color_command
-	case '*'
-		set_color $fish_color_error
-  end
+    # Main
+    echo -n \n(set_color white)@ (set_color yellow)(prompt_pwd)
 
-  echo -n '> '
-  set_color normal
+    # Git
+    set last_status $status
+    printf ' %s ' (set_color normal)(__fish_git_prompt)
+    set_color normal
 
-  # for z.fish
-  z --add "$PWD"
-
-  # for autojump
-  autojump -a $PWD > /dev/null &
+    # Main
+    echo -n \n(set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
 end
