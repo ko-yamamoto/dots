c95bed7be5961f134f95ddb11013817326ba5486
nishikawasasaki <kawaisosu.sosu@gmail.com>
2019-01-13 01:15:03 +0900

    Add dired settings

--- .emacs.d/inits/10_dired.el
+++ .emacs.d/inits/10_dired.el
@@ -82,20 +82,20 @@
 
   ;;   (use-package dired-filter)
 
-  ;; ;; dired のバッファ名末尾に [Dired] を追加する
-  ;; (defun dired-my-append-buffer-name-hint ()
-  ;;   "Append a auxiliary string to a name of dired buffer."
-  ;;   (when (eq major-mode 'dired-mode)
-  ;;     (let* ((dir (expand-file-name list-buffers-directory))
-  ;;            (drive (if (and (eq 'system-type 'windows-nt) ;; Windows の場合はドライブレターを追加
-  ;;                            (string-match "^\\([a-zA-Z]:\\)/" dir))
-  ;;                       (match-string 1 dir) "")))
-  ;;       (rename-buffer (concat (buffer-name) " [" drive "Dired]") t))))
-  ;; (add-hook 'dired-mode-hook 'dired-my-append-buffer-name-hint)
+  ;; dired のバッファ名末尾に [Dired] を追加する
+  (defun dired-my-append-buffer-name-hint ()
+    "Append a auxiliary string to a name of dired buffer."
+    (when (eq major-mode 'dired-mode)
+      (let* ((dir (expand-file-name list-buffers-directory))
+             (drive (if (and (eq 'system-type 'windows-nt) ;; Windows の場合はドライブレターを追加
+                             (string-match "^\\([a-zA-Z]:\\)/" dir))
+                        (match-string 1 dir) "")))
+        (rename-buffer (concat (buffer-name) " [" drive "Dired]") t))))
+  (add-hook 'dired-mode-hook 'dired-my-append-buffer-name-hint)
 
-  ;; ;; ドットファイルなど優先度の低いファイルはデフォルトで非表示とする → アイコンが表示されなくなる
-  ;; (add-hook 'dired-mode-hook
-  ;;           'dired-omit-mode)
+  ;; ドットファイルなど優先度の低いファイルはデフォルトで非表示とする → アイコンが表示されなくなる
+  (add-hook 'dired-mode-hook
+            'dired-omit-mode)
 
   (defun dired-find-file-other-exist-window ()
     "ウィンドウを再利用してもう片方のウィンドウで開く"
