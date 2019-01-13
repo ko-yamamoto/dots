7285c81b66fdaac5df4f738310b65d2017e3b5a0
nishikawasasaki <kawaisosu.sosu@gmail.com>
2019-01-13 14:49:28 +0900

    Remove unused settings

--- .emacs.d/inits/70_git-plugins.el
+++ .emacs.d/inits/70_git-plugins.el
@@ -1,11 +1,3 @@
-;; vcを起動しないようにする
-;; (custom-set-variables
- ;; '(vc-handled-backends nil))
-
-;; 不要なhookを外す
-;; (remove-hook 'find-file-hook 'vc-find-file-hook)
-;; (remove-hook 'kill-buffer-hook 'vc-kill-buffer-hook)
-
 (use-package magit
   :defer t
   :bind (("C-c g g" . magit-status)
