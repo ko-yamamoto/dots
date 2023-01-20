e561e6d7eb8d1e2644e7b5be0310694431b45fe8
nishikawasasaki <kawaisosu.sosu@gmail.com>
2023-01-20 20:25:23 +0900

    Add Emacs themes

--- .emacs.d/inits/70_visual.el
+++ .emacs.d/inits/70_visual.el
@@ -49,6 +49,23 @@
     :config
     (add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode))))
 
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic nil) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  ;; (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-one") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 ;; color-themeの設定
 (require 'color-theme)
 (color-theme-initialize)
