3205e97e524739cc91b664e8072d84d1904eb054
nishikawasasaki <kawaisosu.sosu@gmail.com>
2023-01-21 22:54:48 +0900

    Add Emacs tree-sitter

---
+++ .emacs.d/inits/20_tree-sitter.el
@@ -0,0 +1,9 @@
+(use-package tree-sitter
+  :ensure t
+  :config
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :ensure t)
+
