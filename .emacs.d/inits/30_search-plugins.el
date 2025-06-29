d9b23abf345be6a903093e6d7e4b44c9ed4b05f5
nishikawasasaki <kawaisosu.sosu@gmail.com>
2025-06-29 09:34:14 +0900

    cmigemoの場所をフルパスで指定するようにした

--- .emacs.d/inits/30_search-plugins.el
+++ .emacs.d/inits/30_search-plugins.el
@@ -1,8 +1,9 @@
 (use-package migemo
   :config
-  (setq migemo-command "cmigemo")
+  (setq migemo-command "/opt/homebrew/bin/cmigemo")
   (setq migemo-options '("-q" "--emacs"))
-  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+  ;; (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+  (setq migemo-dictionary "/opt/homebrew/Cellar/cmigemo/20110227/share/migemo/utf-8/migemo-dict")
   (setq migemo-user-dictionary nil)
   (setq migemo-regex-dictionary nil)
   (setq migemo-coding-system 'utf-8-unix)
